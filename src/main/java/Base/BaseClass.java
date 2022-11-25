527bb2def97e2f987b78087bdb520da82a16f20b
kodumuni <72094511+kodumuni@users.noreply.github.com>
2022-11-25 14:54:19 +0530

    Update BaseClass.java
    Change in base class

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -27,7 +27,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.*;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.github.javafaker.Faker;
@@ -91,10 +91,9 @@ public class BaseClass {
 
 	}
 
-	@AfterClass    
-	public void tearDown() throws InterruptedException 
-	{                
-		driver.quit();    
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+	driver.quit();
 	}
 
 	public static void quit() {
